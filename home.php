c6038c67bf05914d9ab11bf2af7b32812a6ce891
Ushbek <scoartnan@gmail.com>
2018-08-30 17:11:59 +0800

    a link
    link

--- home.php
+++ home.php
@@ -8,5 +8,6 @@
 
 	echo "aa";
 ?>
+	<a href="https://www.digitalocean.com/community/tutorials/how-to-install-linux-apache-mysql-php-lamp-stack-on-ubuntu-16-04">Server Installation</a>
 </body>
 </html>
